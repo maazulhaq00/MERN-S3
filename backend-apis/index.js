88d075f4c0a336adc9b975b44d1a31f3af2a31da
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-10-27 11:20:09 +0500

    category model & apis

--- backend-apis/index.js
+++ backend-apis/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectToDB from './config/connectToDb.js'
+import Category from './models/categoryModel.js'
 dotenv.config()
 
 const app = express()
@@ -11,6 +12,21 @@ connectToDB();
 
 app.get("/", (req, res) => {
     return res.status(200).json({message: "welcome to app"})
+})
+
+app.post("/categories", async (req, res) => {
+
+    let {name, description} = req.body
+
+    // const category = await Category.create({name: name, description: description})
+    const category = await Category.create({name, description})
+
+    return res.status(201).json({success: true, category})
+})
+
+app.get("/categories", async (req, res)=>{
+    const categories = await Category.find();
+    return res.status(200).json({success: true, categories  })
 })
 
 
